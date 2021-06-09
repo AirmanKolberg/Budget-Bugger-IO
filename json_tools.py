27e9c45103b110fb345c321fcaee14be1623667b
AirmanKolberg <71570804+AirmanKolberg@users.noreply.github.com>
2021-06-09 14:36:23 -0600

    json data started

---
+++ json_tools.py
@@ -0,0 +1,12 @@
+import json
+
+
+def json_to_dict(json_location):
+    with open(json_location) as f:
+        data = json.load(f)
+    return data
+
+
+def dict_to_json(dictionary, json_location):
+    with open(json_location, 'w') as json_file:
+        json.dump(dictionary, json_file)
