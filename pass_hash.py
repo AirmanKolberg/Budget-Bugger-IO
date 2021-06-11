add05ec315d2d93ee348989a7727ac96f69f8ca1
AirmanKolberg <71570804+AirmanKolberg@users.noreply.github.com>
2021-06-10 21:43:16 -0600

    added pass hash

---
+++ pass_hash.py
@@ -0,0 +1,19 @@
+import hashlib
+
+
+def verify_password(user_in_pass, correct_pass_hash):
+
+    user_in_converted = bytes(user_in_pass, 'utf-8')
+    user_in_hash = hashlib.sha256(user_in_converted).hexdigest()
+
+    if user_in_hash == correct_pass_hash:
+        return True
+    return False
+
+
+def create_new_password(user_in):
+
+    converted_password = bytes(user_in, 'utf-8')
+    password_hash = hashlib.sha256(converted_password).hexdigest()
+
+    return password_hash
