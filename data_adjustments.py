6347bc7294140969b4c1cad6455a9fc430594534
AirmanKolberg <71570804+AirmanKolberg@users.noreply.github.com>
2021-06-11 20:23:48 -0600

    added file for adjusting backend data

---
+++ data_adjustments.py
@@ -0,0 +1,41 @@
+from system_commands import get_current_time_and_date
+from user_inputs import get_float_value_from_user
+
+"""
+Note:
+    
+When any adjustments are made to the json file,
+everything must be recalculated on the backend.
+"""
+
+
+# Add an adjustment or transaction (denoted as a string)
+def add_adjustment_or_transaction(json_dict, new_data,
+                                  adjustment_or_transaction):
+
+    json_dict[adjustment_or_transaction].append(new_data)
+
+
+def create_adjustment_or_transaction(adjustment_or_transaction):
+
+    now = get_current_time_and_date()
+
+    # Get transaction/adjustment detail
+    note = input('Note: ')
+
+    amount = get_float_value_from_user(
+        f'{adjustment_or_transaction.title()} amount: ')
+
+    # Invert transactions, as they are to be subtracted
+    if adjustment_or_transaction == 'transaction':
+        amount *= -1
+
+    # Format the new transaction/adjustment
+    data = {amount: [now, note]}
+
+    return data
+
+
+# Calculates the daily current budget
+def determine_daily_shift():
+    print('code this')
