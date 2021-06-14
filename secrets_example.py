d7c60c9474bacf494f2eb071ac42b550e47836dd
AirmanKolberg <71570804+AirmanKolberg@users.noreply.github.com>
2021-06-14 08:31:14 -0600

    included position information

--- secrets_example.py
+++ secrets_example.py
@@ -8,8 +8,8 @@ usaa_user = 'username_here'
 usaa_pass = 'your_pass-can-kiss-my_hash'
 
 usaa_csv_location = {
-    'PyCharmProjects': [567, 322],
-    'MyFavouriteOne': [567, 390]
+    'PyCharmProjects': [567, 322],  # Coordinates of upper-left result
+    'MyFavouriteOne': [567, 390]    # Coordinates of lower-left result
 }
 
 
