3b0c93202442205b588f11b96bef603974e89a9b
T.J. Kolberg <tkolberg@asu.edu>
2021-07-16 21:19:26 -0600

    🗣  Grab User Information

--- user_inputs.py
+++ user_inputs.py
@@ -2,9 +2,13 @@
 def verify_yes_or_no(response):
 
     if response == 'yes' or response == 'y':
+        
         return True
+
     elif response == 'no' or response == 'n':
+
         return False
+
     else:
         response = input("Try again, simple 'yes', 'y', 'no', or 'n': ")
         verify_yes_or_no(response)
@@ -14,8 +18,11 @@ def verify_yes_or_no(response):
 def get_float_value_from_user(message_prompt):
 
     try:
+
         value = float(input(message_prompt))
+
     except ValueError:
+
         print('Please try again, numbers only...')
         value = get_float_value_from_user(message_prompt)
 
@@ -26,8 +33,11 @@ def get_float_value_from_user(message_prompt):
 def get_int_value_from_user(message_prompt):
 
     try:
+
         value = int(input(message_prompt))
+
     except ValueError:
+        
         print('Please try again, integers only...')
         value = get_int_value_from_user(message_prompt)
 
@@ -35,4 +45,5 @@ def get_int_value_from_user(message_prompt):
 
 
 if __name__ == '__main__':
+
     pass
